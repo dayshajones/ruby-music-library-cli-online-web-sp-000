4e3b82e8430576cb54e1d122c8e867c9dc89c9cd
Daysha Jones <daysha.vu@icloud.com>
2021-03-22 20:05:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ class Artist
 
     def add_song(song)
         song.artist = self unless song.artist
-        @songs << song unless songs.include?(song)
+        songs << song unless songs.include?(song)
     end
 
     def genres
